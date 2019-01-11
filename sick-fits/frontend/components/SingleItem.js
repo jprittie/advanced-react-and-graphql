c94edfee18360fce59a65ca0b5c26482e12dc44c
jprittie <eyewallop@gmail.com>
2019-01-10 22:40:17 -0500

    Add title and additional styling to SingleItem

--- sick-fits/frontend/components/SingleItem.js
+++ sick-fits/frontend/components/SingleItem.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import Error from './ErrorMessage';
 import styled from 'styled-components';
+import Head from 'next/head';
 
 const SingleItemStyles = styled.div`
     max-width: 1200px;
@@ -12,6 +13,15 @@ const SingleItemStyles = styled.div`
     grid-auto-columns: 1fr;
     grid-auto-flow: column;
     min-height: 800px;
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+    .details {
+        margin: 3rem;
+        font-size: 2rem;
+    }
 `
 
 const SINGLE_ITEM_QUERY = gql`
@@ -40,7 +50,16 @@ export class SingleItem extends Component {
             if (!data.item) return <p>No item found for {this.props.id}.</p>;
             const item = data.item;
             return <SingleItemStyles>
+                <Head>
+                    <title>
+                        Sick Fits | {item.title}
+                    </title>
+                </Head>
                 <img src={item.largeImage} alt={item.title}/>
+                <div className="details">
+                    <h2>Viewing {item.title}</h2>
+                    <p>{item.description}</p>
+                </div>
             </SingleItemStyles>
         }}
       </Query>
