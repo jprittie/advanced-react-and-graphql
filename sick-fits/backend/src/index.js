f34c998151fb50fa283066f55a36d03177be17a1
jprittie <eyewallop@gmail.com>
2019-01-12 22:24:36 -0500

    Add cookieParser

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,3 +1,5 @@
+const cookieParser = require('cookie-parser');
+
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -5,6 +7,12 @@ const db = require('./db');
 const server = createServer();
 
 // TODO use express middleware to handle cookies (JWT)
+// server.express.use() allows us to use any existing express middleware
+// we'll pass it an instance of cookieParser
+// this will allow us to access cookies as a nice formatted object, rather than just a cookie string that it normally comes in as a header
+server.express.use(cookieParser());
+// we will be accepting the request, then parsing any cookies that came along with the request
+// so that we can parse that JWT and be able to authenticate the current user
 // TODO use express middleware to populate current user
 
 server.start({
