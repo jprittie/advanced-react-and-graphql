ee8102be17e50089a4bafb3c410eed6aa7687fbe
jprittie <eyewallop@gmail.com>
2019-01-17 20:34:49 -0500

    Display authorized section of Nav

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -3,33 +3,37 @@ import NavStyles from './styles/NavStyles';
 import User from './User';
 
 const Nav = () => (
-    <NavStyles>
-      <User>
-         {/* use double destructure to access payload.data.me */}
-         {({ data: { me } }) => {
-            console.log(me);
-            // console.log(data);
-            // return <p>User</p>;
-            if (me) return <p>{me.name}</p>
-            return null
-         }}
-      </User>
-      <Link href="/items">
-         <a>Shop</a>
-      </Link>
-      <Link href="/sell">
-         <a>Sell</a>
-      </Link>
-      <Link href="/signup">
-         <a>Signup</a>
-      </Link>
-      <Link href="/orders">
-         <a>Orders</a>
-      </Link>
-      <Link href="/me">
-         <a>Account</a>
-      </Link>
-    </NavStyles>
+   <User>
+      {/* use double destructure to access payload.data.me */}
+      {({ data: { me } }) => (
+         <NavStyles>
+            <Link href="/items">
+               <a>Shop</a>
+            </Link>
+            {me &&
+               <>
+                  <Link href="/sell">
+                     <a>Sell</a>
+                  </Link>
+                  <Link href="/orders">
+                     <a>Orders</a>
+                  </Link>
+                  <Link href="/me">
+                     <a>Account</a>
+                  </Link>
+               </>
+            }
+
+            {!me &&
+               <>
+                  <Link href="/signup">
+                     <a>Sign In</a>
+                  </Link>
+               </>
+            }
+         </NavStyles>
+      )}
+   </User>
 )
 
 export default Nav;
