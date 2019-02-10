ea148a37154435b2abcee3427ed6cce75c6febc1
jprittie <eyewallop@gmail.com>
2019-02-10 17:27:59 -0500

    Add Apollo local state queries and mutations

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,39 +1,38 @@
-import Nav from './Nav';
 import Link from 'next/link';
 import styled from 'styled-components';
-import Router from 'next/router';
 import NProgress from 'nprogress';
+import Router from 'next/router';
+import Nav from './Nav';
+import Cart from './Cart';
 
 Router.onRouteChangeStart = () => {
-    NProgress.start();
-}
-
+  NProgress.start();
+};
 Router.onRouteChangeComplete = () => {
-    NProgress.done();
-}
+  NProgress.done();
+};
 
-Router.onRouteChangeEror = () => {
-    NProgress.done();
-}
-
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 
 const Logo = styled.h1`
-    font-size: 4rem;
-    margin-left: 2rem;
-    position: relative;
-    z-index: 2;
-    transform: skew(-7deg);
-    a {
-        padding: 0.5rem 1rem;
-        background: ${props => props.theme.red};
-        color: white;
-        text-transform: uppercase;
-        text-decoration: none;
-    }
-    @media (max-width: 1300px) {
-        margin: 0;
-        text-align: center;
-    }
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    padding: 0.5rem 1rem;
+    background: ${props => props.theme.red};
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
 `;
 
 const StyledHeader = styled.header`
@@ -55,22 +54,21 @@ const StyledHeader = styled.header`
   }
 `;
 
-
 const Header = () => (
-    <StyledHeader>
-        <div className="bar">
-            <Logo>
-                <Link href="/">
-                    <a>Sick Fits</a>
-                </Link>
-            </Logo>
-            <Nav />
-        </div>
-        <div className="sub-bar">
-            <p>Search</p>
-        </div>
-        <div>Cart</div>
-    </StyledHeader>
-)
+  <StyledHeader>
+    <div className="bar">
+      <Logo>
+        <Link href="/">
+          <a>Sick Fits</a>
+        </Link>
+      </Logo>
+      <Nav />
+    </div>
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <Cart />
+  </StyledHeader>
+);
 
 export default Header;
--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { Mutation } from 'react-apollo';
+import { TOGGLE_CART_MUTATION } from './Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
@@ -8,34 +10,37 @@ const Nav = () => (
       {/* use double destructure to access payload.data.me */}
       {({ data: { me } }) => (
          <NavStyles>
-            <Link href="/items">
-               <a>Shop</a>
-            </Link>
-            {me &&
-               <>
-                  <Link href="/sell">
-                     <a>Sell</a>
-                  </Link>
-                  <Link href="/orders">
-                     <a>Orders</a>
-                  </Link>
-                  <Link href="/me">
-                     <a>Account</a>
-                  </Link>
-                  <Signout />
-               </>
-            }
+         <Link href="/items">
+           <a>Shop</a>
+         </Link>
+         {me && (
+           <>
+             <Link href="/sell">
+               <a>Sell</a>
+             </Link>
+             <Link href="/orders">
+               <a>Orders</a>
+             </Link>
+             <Link href="/me">
+               <a>Account</a>
+             </Link>
+             <Signout />
+             <Mutation mutation={TOGGLE_CART_MUTATION}>
+               {(toggleCart) => (
+                 <button onClick={toggleCart}>My Cart</button>
+               )}
+             </Mutation>
+           </>
+         )}
+         {!me && (
+           <Link href="/signup">
+             <a>Sign In</a>
+           </Link>
 
-            {!me &&
-               <>
-                  <Link href="/signup">
-                     <a>Sign In</a>
-                  </Link>
-               </>
-            }
-         </NavStyles>
-      )}
+         )}
+       </NavStyles>
+     )}
    </User>
-)
+ );
 
-export default Nav;
+ export default Nav;
